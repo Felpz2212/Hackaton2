2729c26bc80f47953cb836f79ff6f012b7bc49df
Felipe Araujo De Almeida <88008127+Felpz2212@users.noreply.github.com>
2024-03-16 13:09:04 -0300

    Update hackaton.js

--- hackaton.js
+++ hackaton.js
@@ -11,10 +11,30 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const { Pool } = require('pg');
+
+  const pool = new Pool({
+    user: 'deploy',
+    host: 'dpg-cnqpunen7f5s7388smlg-a.oregon-postgres.render.com',
+    database: 'dbhackaton_vs6u',
+    password: 'E8AasENMyCvyjLaPNjKw52Hwi0NnmKvq',
+    port: '5432',
+  });
+
+  pool.end();
+
 app.post('/dialogflow', async (req, res) => {
 
   console.log('Entrou')
 
+
+  pool.query('SELECT * from NF', (err, value) => {
+    if (err) {
+      console.error('Error executing query', err);
+      return;
+    }
+    console.log('Current date from PostgreSQL:', value.rows);
+  });
 
   const NF = await prisma.NF.findOne({where:{
       documento: req.body.documento
